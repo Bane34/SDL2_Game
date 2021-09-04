6d916e23bb3259db958f10937f458367ea2912b0
Bane34 <bane34.42@gmail.com>
2021-09-04 11:00:22 +0200

    Rendering stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <iostream>
 
@@ -10,42 +11,69 @@ int main(int argc, char** argv){
 
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
-    SDL_Event event;
+
+    //--- Init SDL2 and SDL2_image ---//
 
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
         printf("SDL couldnt init! SDL_ERROR: %s\n", SDL_GetError());
         return -1;
     }
+    IMG_Init(IMG_INIT_PNG);
 
-    // Creating the window
+    //--- Create the window and the renderer ---//
+
     window = SDL_CreateWindow("SDL tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     if(!window){
         printf("Error creating the window! SDL_ERROR: %s\n", SDL_GetError());
         return -1;
     }
     
-    int r = 0, g = 0, b = 0;
-
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(!renderer){
+        printf("Error creating the renderer! SDL_ERROR: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    //--- Loading a texture onto the screen ---//
+    SDL_Texture* texture = NULL;
+    SDL_Surface* temp = IMG_Load("images/stickman.png");
+
+    texture = SDL_CreateTextureFromSurface(renderer, temp);
+    SDL_FreeSurface(temp);
+
+    // To load the texture we need to define
+    // A SDL_Rect with his dimensions and position
+    SDL_Rect rect;
+    rect.x = 0;
+    rect.y = 200;
+    rect.w = 107;
+    rect.h = 240;
+
+    //--- Loop of the screen ---//
+
+    SDL_Event event;
+
     while(running){
         while(SDL_PollEvent(&event)){
             if (event.type == SDL_QUIT)
                 running = false;
-            else if(event.type == SDL_KEYDOWN){
-                r = rand() % 255;
-                g = rand() % 255;
-                b = rand() % 255;
-            }
-
         }
 
-        SDL_SetRenderDrawColor(renderer, r, g, b, 0xFF);
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, &rect);
         SDL_RenderPresent(renderer);
     }    
 
+    //--- Destroying the objects ---//
+
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
+
+    //--- Quitting the program ---//
+
+    IMG_Quit();
     SDL_Quit();
 
     return 0;
