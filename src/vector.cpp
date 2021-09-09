0467be581669cb09d99e3483f44346821830b625
Bane34 <bane34.42@gmail.com>
2021-09-09 22:53:13 +0200

    created vectors structs

---
+++ src/vector.cpp
@@ -0,0 +1,21 @@
+#include "vectors.h"
+
+Vector2i::Vector2i(){
+    x = 0;
+    y = 0;
+}
+
+Vector2i::Vector2i(int p_x, int p_y){
+    x = p_x;
+    y = p_y;
+}
+
+Vector2f::Vector2f(){
+    x = 0.f;
+    y = 0.f;
+}
+
+Vector2f::Vector2f(float p_x, float p_y){
+    x = p_x;
+    y = p_y;
+}
