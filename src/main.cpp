3b5c41f9656d35fc92a672d6a6225d26088fd64b
Bane34 <bane34.42@gmail.com>
2021-11-26 23:43:20 +0100

    imgui example in the window

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,23 @@ int main(int, char**){
 
     window.getVersion();
 
+    ImVec4 clearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
     while(window.isOpen()){
         window.handleEvents();
 
         window.imGuiCreate();
-        ImGui::ShowDemoWindow();
+        {
+            ImGui::Begin("Color changer");
+            ImGui::Text("Usefull text");
+            ImGui::ColorEdit3("clear color", (float*) &clearColor);
+            ImGui::NewLine();
+            ImGui::Text("Application aveage %.3f ms/frame (%.1f FPS)", 1000.f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
         window.imGuiRender();
 
-        window.clear({0xFF, 0xFF, 0xFF, 0xFF});
+        window.clear({(uint8_t) (clearColor.x * 255), (uint8_t) (clearColor.y * 255), (uint8_t) (clearColor.z *  255), (uint8_t) (clearColor.w * 255)});
         window.render();
         window.windowDraw();
     }
